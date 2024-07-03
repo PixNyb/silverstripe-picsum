d42f905b79e5005a2b7957d2b140f39da96de365
PixNyb <contact@roelc.me>
2024-07-03 09:22:25 +0000

    Updated build task

--- src/Tasks/DownloadSampleImageTask.php
+++ src/Tasks/DownloadSampleImageTask.php
@@ -4,13 +4,18 @@ namespace PixNyb\Picsum\Tasks;
 
 use SilverStripe\Dev\BuildTask;
 use SilverStripe\Assets\File;
+use SilverStripe\Assets\Flysystem\FlysystemAssetStore;
 use SilverStripe\Assets\Image;
 use SilverStripe\Control\Director;
-use SilverStripe\Core\Path;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\Assets\Storage\FileHashingService;
+use Exception;
+use SilverStripe\ORM\DB;
 
 class DownloadSampleImageTask extends BuildTask
 {
     private static $segment = 'download-sample-images';
+    private static $assets_dir = 'assets';
 
     protected $title = 'Download Sample Images';
     protected $description = 'Download sample images from Lorem Picsum for each image that doesn\'t exist on disk';
@@ -20,11 +25,22 @@ class DownloadSampleImageTask extends BuildTask
     {
         // Get all images
         $images = File::get()->filter('ClassName', Image::class);
+        echo "Found " . $images->count() . " images\n";
         $counter = 0;
 
         foreach ($images as $image) {
-            $filename = $image->FileFilename;
-            $path = Director::publicFolder() . '/assets/' . $filename;
+            $filename = $image->getField('FileFilename');
+            if (!$filename) {
+                $record = DB::query("SELECT Filename FROM File WHERE ID = " . $image->ID)->value();
+                if ($record)
+                    $filename = $record;
+            }
+
+            echo "Checking $filename\n";
+            if (!$filename)
+                continue;
+
+            $path = Director::publicFolder() . $filename;
             $dir = dirname($path);
 
             if (!file_exists($path)) {
@@ -35,13 +51,28 @@ class DownloadSampleImageTask extends BuildTask
                 $height = rand(500, 1000);
 
                 $url = 'https://picsum.photos/' . $width . '/' . $height;
+
                 $data = file_get_contents($url);
                 file_put_contents($path, $data);
-                $image->setFromLocalFile($path);
-                $image->updateFilesystem();
 
                 $counter++;
                 echo "Downloaded $filename\n";
+            }
+
+            try {
+                // Strip the assets dir before putting it in FileFilename
+                $filefilename = preg_replace('/^' . preg_quote($this->config()->get('assets_dir') ?? 'assets', '/') . '\//', '', $filename);
+                $image->setField('FileFilename', $filefilename);
+
+                $hash = Injector::inst()->get(FileHashingService::class)->computeFromStream(fopen($path, 'r'));
+                $image->setField('FileHash', $hash);
+
+                $image->write();
+
+                $image->updateFilesystem();
+                $image->write();
+            } catch (Exception $e) {
+                echo "Error updating $filename\n";
             }
         }
 
